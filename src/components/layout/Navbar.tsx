99248c066a3aed59292cd688acf10081820bcf10
Matheus <devmatheus12@gmail.com>
2023-07-30 17:44:48 -0300

    re-up configurações de deploy

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -9,9 +9,9 @@ export default function Navbar({home}: NavbarOptions) {
     <div className={home? 'bg-transparent':'bg-white'}>
       <div className="flex max-w-7xl mx-auto justify-end py-6 bg">
         <ul className="flex w-[30%] justify-between">
-          <li><Link className="navbar-font text-[22px]" to="/">Home</Link></li>
-          <li><Link className="navbar-font text-[22px]" to="projects">Projetos</Link></li>
-          <li><Link className="navbar-font text-[22px]" to="/">Sobre mim</Link></li>
+          <li><Link className="navbar-font text-[22px]" to="landing-page-matheus">Home</Link></li>
+          <li><Link className="navbar-font text-[22px]" to="projects">Projects</Link></li>
+          <li><Link className="navbar-font text-[22px]" to="about-me">About me</Link></li>
         </ul>
       </div>
     </div>
